9c82cf8d43b5e8fee0113fa7f523b084a6545df7
higgipoker <paul.samuel.higgins@gmail.com>
2020-11-04 20:42:39 +0100

    update build script

--- scripts/setup_linux.sh
+++ scripts/setup_linux.sh
@@ -1,14 +1,21 @@
 #!/bin/bash
-BUILD_TYPE=$1
-tr [a-z] [A-Z] < $BUILD_TYPE
 
-if [ "$BUILD_TYPE" = "DEBUG" ]; then
-    BUILD_TYPE = "Debug"
-else
-    BUILD_TYPE = "Release"
+BUILD_TYPE=$1
+
+if [ -z "$BUILD_TYPE" ]
+  then
+    BUILD_TYPE="debug"
 fi
 
-BUILD_DIR="build_"$1
+tr [a-z] [A-Z] < $BUILD_TYPE
+
+if [ "$BUILD_TYPE" = "RELEASE" ]; then
+    BUILD_TYPE = "Release"
+else
+    BUILD_TYPE = "Debug"
+fi
+
+BUILD_DIR="build_"$BUILD_TYPE
 
 cd ..;
 mkdir $BUILD_DIR;
